4e398f76c819f277c40df007376ea9ed09b74fea
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2025-07-25 18:41:03 +0200

    AWS textract fix

--- benchmarkdown/textract.py
+++ benchmarkdown/textract.py
@@ -3,6 +3,7 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
 from textractor import Textractor
+from textractor.data.constants import TextractFeatures
 
 class TextractMarkdownExtractor:
     """
@@ -32,12 +33,13 @@ class TextractMarkdownExtractor:
             """
             Blocking function to get text from the document.
             """
-            document = self.extractor.start_document_text_detection(
+            document = self.extractor.start_document_analysis(
                 str(filename),
+                features=[TextractFeatures.LAYOUT, TextractFeatures.TABLES, TextractFeatures.FORMS, TextractFeatures.SIGNATURES],
                 s3_upload_path=self.s3_upload_path,
                 save_image=False,
             )
-            return document.get_text()
+            return document.to_markdown()
 
         loop = asyncio.get_running_loop()
         result = await loop.run_in_executor(None, blocking_get_text, filename)
